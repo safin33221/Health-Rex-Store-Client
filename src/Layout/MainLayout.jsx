4f2a4efacdce2784074d37e6b49cbceb658d2f05
safin33221 <saifn33221@gmail.com>
2025-01-13 21:23:59 +0600

    completed footer section

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,19 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar";
+import Footer from "../Pages/Shared/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <nav>
-                <Navbar/>
+                <Navbar />
             </nav>
+            <main className="min-h-screen">
+                <Outlet />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
